7b09e50adea87bc93b6472f85938184df1ba23df
kaygizzle <mgoqikhangelani@gmail.com>
2024-07-25 09:47:11 +0200

    base working version

--- code.js
+++ code.js
@@ -1,1 +1,33 @@
-throw new Error("This plugin template uses TypeScript. Follow the instructions in `README.md` to generate `code.js`.")
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+figma.showUI(__html__);
+figma.ui.onmessage = (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    if (msg.type === 'track-elements') {
+        const nodes = figma.currentPage.children;
+        const elements = [];
+        function traverse(node, depth = 0) {
+            elements.push({
+                id: node.id,
+                name: node.name,
+                type: node.type,
+                depth: depth,
+                childrenCount: 'children' in node ? node.children.length : 0
+            });
+            if ('children' in node) {
+                for (const child of node.children) {
+                    traverse(child, depth + 1);
+                }
+            }
+        }
+        nodes.forEach(node => traverse(node));
+        figma.ui.postMessage({ type: 'tracked-elements', elements: elements });
+    }
+});
